f3c0bb9f935973c1c579ba6e2c9b9292ed91abcd
Blue Sky <neelavanancoder@gmail.com>
2026-07-16 16:49:28 +0530

    Time: 3 ms (66.1%), Space: 19.3 MB (23.4%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.py
@@ -0,0 +1,17 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        if x < 2:
+            return x
+        
+        low, high = 1, x
+        ans = 0
+        
+        while low <= high:
+            mid = low + (high - low) // 2
+            if mid <= x // mid:
+                ans = mid
+                low = mid + 1
+            else:
+                high = mid - 1
+                
+        return ans
